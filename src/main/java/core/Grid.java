9584d61c424cc1c52911fca948df075489d93c7f
MorganChG <gatliffmorgan6@gmail.com>
2024-10-22 15:29:48 -0400

    Make the Grid Class and Implement its functions

--- src/main/java/core/Grid.java
+++ src/main/java/core/Grid.java
@@ -1,15 +1,40 @@
 package core;
 
+import java.util.List;
+
 public class Grid {
+    private final int rows;
+    private final int cols;
+
+    private Cell[][] grid;
+
+    private List<Cell> shipList;
+    private List<Cell> chosenCells;
+
+    public Grid(int rows, int cols) {
+
+        this.rows = rows;
+        this.cols = cols;
+
+        grid = new Cell[rows][cols];
+
+        for (int i = 0 ; i < cols ; i++) {
+            for (int j = 0 ; j < rows ; j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+
+    }
+
     public Cell get(int row, int col) {
-        return null;
+        return grid[col][row];
     }
 
     public int numRows() {
-        return 0;
+        return rows;
     }
 
     public int numCols() {
-        return 0;
+        return cols;
     }
 }
