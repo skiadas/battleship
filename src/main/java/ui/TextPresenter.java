e0f67c6ffe21991f7c3ac5da2bfc2518b5946176
Oxiel <castrobadilloo27@hanover,edu>
2024-10-23 15:04:42 -0400

    "Created a visualization of the Grid"

--- src/main/java/ui/TextPresenter.java
+++ src/main/java/ui/TextPresenter.java
@@ -17,5 +17,33 @@ public class TextPresenter implements Presenter {
         System.out.println(s);
     }
 
-    public void displayGrid(Grid g) {}
+    public void displayGrid(Grid g) {
+        // These gets us the dimensions of the grid
+        int numOfRows = g.numRows();
+        int numOfCols = g.numCols();
+        String[] letter = {
+            " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+        // now we need to display the grid using its rows and colunms
+        // while also displaying the numbers of the x-axis and letters on the y-axis
+        for (int num = 0; num < numOfRows; num++) {
+            if (num == 0) {
+                System.out.print("   ");
+            }
+            if (num < 10) {
+                System.out.print("  " + (num + 1) + " ");
+            } else {
+                System.out.print(" " + (num + 1) + " ");
+            }
+        }
+        System.out.println("\n");
+        for (int row = 1; row < numOfRows + 1; row++) {
+            System.out.print(" " + (letter[row]) + " ");
+            for (int col = 1; col < numOfCols + 1; col++) {
+                System.out.print("  " + g.get(row, col) + " ");
+            }
+            System.out.println("\n");
+        }
+    }
 }
