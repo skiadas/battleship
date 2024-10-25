eb00c69aec6435d88a54acd2bf326d2002d5f5fa
MorganChG <gatliffmorgan6@gmail.com>
2024-10-25 15:50:04 -0400

    Fixed Grid Class

--- src/test/java/core/GridTest.java
+++ src/test/java/core/GridTest.java
@@ -5,18 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class GridTest {
-    Grid testGrid = new Grid(3, 3);
+    Grid testGrid = new Grid(3, 2);
 
     @Test
     public void aNewGridHasProvidedDimensions() {
         assertEquals(testGrid.numRows(), 3);
-        assertEquals(testGrid.numCols(), 3);
+        assertEquals(testGrid.numCols(), 2);
     }
 
     @Test
     public void aNewGridHasNoneNullCells() {
-        for (int i = 0; i < testGrid.numCols(); i++) {
-            for (int j = 0; j < testGrid.numRows(); j++) {
+        for (int i = 0; i < testGrid.numRows(); i++) {
+            for (int j = 0; j < testGrid.numCols(); j++) {
                 assertNotEquals(null, testGrid.get(i, j));
             }
         }
