b0d353f4779172f57d9931de676e0a991a61431a
raihanm26 <mdhamimraihan@Mds-MacBook-Air.local>
2024-10-29 15:37:59 -0400

    Reformatted

--- src/main/java/core/Cell.java
+++ src/main/java/core/Cell.java
@@ -1,4 +1,4 @@
-//Raihan and Jonah
+// Raihan and Jonah
 package core;
 
 public class Cell {
@@ -7,14 +7,12 @@ public class Cell {
     private boolean hit;
     private boolean miss;
 
-
     public Cell() {
         this.shot = false;
         this.ship = false;
         this.hit = false;
         this.miss = false;
     }
-
 
     public Cell(boolean ship) {
         this.ship = ship;
@@ -23,33 +21,33 @@ public class Cell {
         this.miss = false;
     }
 
-
     public boolean isEmpty() {
         return !this.hasShip();
     }
 
-    //the cell has been shot
+    // the cell has been shot
     public boolean hasBeenShot() {
         return this.shot;
     }
 
-    //the cell contains a ship
+    // the cell contains a ship
     public boolean hasShip() {
         return this.ship;
     }
 
-    //the shot on this cell resulted in a hit
+    // the shot on this cell resulted in a hit
     public boolean cellIsHit() {
         return this.hit;
     }
 
-    //shot on this cell resulted in a miss
+    // shot on this cell resulted in a miss
     public boolean cellIsMiss() {
         return this.miss;
     }
-    //shot and hit if it contains a ship
+
+    // shot and hit if it contains a ship
     public void hit() {
-        if (!hasBeenShot() && hasShip()) {  // Ensure shot hasn't been made yet
+        if (!hasBeenShot() && hasShip()) { // Ensure shot hasn't been made yet
             setAsShot();
             setAsHit();
         }
@@ -57,7 +55,7 @@ public class Cell {
 
     // Handle a miss on the cell
     public void miss() {
-        if (!hasBeenShot() && !hasShip()) {  // Ensure shot hasn't been made yet
+        if (!hasBeenShot() && !hasShip()) { // Ensure shot hasn't been made yet
             setAsShot();
             setAsMiss();
         }
@@ -83,7 +81,7 @@ public class Cell {
         this.hit = true;
     }
 
-    //Reset the cell to its initial state
+    // Reset the cell to its initial state
     public void reset() {
         this.shot = false;
         this.ship = false;
