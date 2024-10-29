1d96f8c7abbf9876952efde1d8cc088aa442091b
Oxiel <castrobadilloo27@hanover,edu>
2024-10-29 14:22:41 -0400

    "Created a visualization of the Grid with spotless done"

--- src/test/java/ui/TextPresenterTest.java
+++ src/test/java/ui/TextPresenterTest.java
@@ -1,48 +1,51 @@
 package ui;
 
-import core.Grid;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import core.Grid;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TextPresenterTest {
     @Test
-    void whenDisplayGridIsCalled_TheGridIsSentToTheOutputStream(){
+    void whenDisplayGridIsCalled_TheGridIsSentToTheOutputStream() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream out = new PrintStream(baos,true,StandardCharsets.UTF_8);
+        PrintStream out = new PrintStream(baos, true, StandardCharsets.UTF_8);
         InputStream in = System.in;
-        Grid grid = new Grid(3,3);
-        TextPresenter presenter = new TextPresenter(out,in);
+        Grid grid = new Grid(3, 3);
+        TextPresenter presenter = new TextPresenter(out, in);
         presenter.displayGrid(grid);
-        String expected = "     1   2   3 \n" +
-                "\n" +
-                " A   -   -   - \n" +
-                "\n" +
-                " B   -   -   - \n" +
-                "\n" +
-                " C   -   -   - \n" + "\n";
+        String expected =
+                "     1   2   3 \n"
+                        + "\n"
+                        + " A   -   -   - \n"
+                        + "\n"
+                        + " B   -   -   - \n"
+                        + "\n"
+                        + " C   -   -   - \n"
+                        + "\n";
         String actual = baos.toString(StandardCharsets.UTF_8);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
-    void whenDisplayGridIsCalled_CreatesRectangularGrid(){
+    void whenDisplayGridIsCalled_CreatesRectangularGrid() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream out = new PrintStream(baos,true,StandardCharsets.UTF_8);
-        Grid grid = new Grid(2,3);
-        TextPresenter presenter = new TextPresenter(out,null);
+        PrintStream out = new PrintStream(baos, true, StandardCharsets.UTF_8);
+        Grid grid = new Grid(2, 3);
+        TextPresenter presenter = new TextPresenter(out, null);
         presenter.displayGrid(grid);
-        String expected = "     1   2   3 \n" +
-                "\n" +
-                " A   -   -   - \n" +
-                "\n" +
-                " B   -   -   - \n" +
-                "\n";
+        String expected =
+                "     1   2   3 \n"
+                        + "\n"
+                        + " A   -   -   - \n"
+                        + "\n"
+                        + " B   -   -   - \n"
+                        + "\n";
         String actual = baos.toString(StandardCharsets.UTF_8);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
