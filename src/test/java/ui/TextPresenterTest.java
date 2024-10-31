d0d84b58a058b9333664e6cfb2cfb7b17f0e4f13
Oxiel <castrobadilloo27@hanover,edu>
2024-10-31 15:55:45 -0400

    Created a askForCoordinate method in TextPresenter along with tests. Along with a isValid method in Grid along with tests

--- src/test/java/ui/TextPresenterTest.java
+++ src/test/java/ui/TextPresenterTest.java
@@ -2,6 +2,7 @@ package ui;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import core.Coord;
 import core.Grid;
 import org.junit.jupiter.api.Test;
 
@@ -38,5 +39,30 @@ class TextPresenterTest {
                         + " B   -   -   - \n"
                         + "\n";
         assertEquals(expected, ioProvider.getOutput());
+    }
+
+    @Test
+    void whenAskForCoordinateItReturnsIT() {
+        Grid grid = new Grid(2, 3);
+        TestIOProvider ioProvider = TestIOProvider.withInput("B3\n");
+        TextPresenter presenter = new TextPresenter(ioProvider);
+        Coord expected = new Coord(2, 3);
+        Coord actual = presenter.askForCoordinate(grid);
+        boolean Result = expected.isEqual(actual);
+        assertEquals(true, Result);
+        assertEquals(expected.row, actual.row);
+        assertEquals(expected.col, actual.col);
+    }
+    @Test
+    void whenAskForCoordinateIsFalse() {
+        Grid grid = new Grid(2, 3);
+        TestIOProvider ioProvider = TestIOProvider.withInput("C5\nB3\n");
+        TextPresenter presenter = new TextPresenter(ioProvider);
+        Coord expected = new Coord(2, 3);
+        Coord actual = presenter.askForCoordinate(grid);
+        boolean Result = expected.isEqual(actual);
+        String expected_m = "Not within the Grid!";
+        assertEquals(expected_m, ioProvider.getOutput());
+        assertEquals(true, Result);
     }
 }
