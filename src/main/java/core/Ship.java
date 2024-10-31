e26ee076aeb7582a668747597d182833e6279ee8
Lily-Kate <156015495+Lily-Kate@users.noreply.github.com>
2024-10-31 15:33:16 -0400

    New Ship function, isSunk(), with test

--- src/main/java/core/Ship.java
+++ src/main/java/core/Ship.java
@@ -77,5 +77,4 @@ public class Ship {
         }
         return true;
     }
-
 }
--- src/test/java/core/ShipTest.java
+++ src/test/java/core/ShipTest.java
@@ -48,7 +48,7 @@ class ShipTest {
 
     @Test
     public void isSunkReturnsTrueIfAllCellsMarkedAsHit() {
-        Grid g  = new Grid(5, 5);
+        Grid g = new Grid(5, 5);
         Coord c1 = new Coord(1, 2);
         Coord c2 = new Coord(2, 2);
         Coord c3 = new Coord(3, 2);
