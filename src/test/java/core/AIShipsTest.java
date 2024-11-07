92b95513b9d109ffc61cdea5a3ff0905437fcf0a
MorganChG <gatliffmorgan6@gmail.com>
2024-11-07 15:03:33 -0500

    Created Ship Spec Class for AI

--- src/test/java/core/AIShipsTest.java
+++ src/test/java/core/AIShipsTest.java
@@ -9,18 +9,27 @@ class AIShipsTest {
     @Test
     public void canPlaceShipsFromSetSizes() {
         Grid grid = new Grid(8, 8);
-        int[] shipSizes = {5, 4, 3, 2, 2, 1};
-        AIShips aiShips = new AIShips(grid, shipSizes);
+        ShipSpec[] shipSpecs =
+                new ShipSpec[] {
+                    new ShipSpec("Carrier", 5), new ShipSpec("Ship", 5), new ShipSpec("Ship", 5)
+                };
+        AIShips aiShips = new AIShips(grid, shipSpecs);
         aiShips.setShips();
-        assertEquals(6, aiShips.getShipCount());
+        assertEquals(3, aiShips.getShipCount());
     }
 
     @Test
     public void throwsErrorIfShipSizeIsTooBig() {
         try {
             Grid grid = new Grid(8, 8);
-            int[] shipSizes = {9, 4, 3, 2, 2, 1};
-            AIShips aiShips = new AIShips(grid, shipSizes);
+            ShipSpec[] shipSpecs =
+                    new ShipSpec[] {
+                        new ShipSpec("Carrier", 5),
+                        new ShipSpec("BattleShip1", 7),
+                        new ShipSpec("BattleShip2", 8),
+                        new ShipSpec("BattleShip3", 9)
+                    };
+            AIShips aiShips = new AIShips(grid, shipSpecs);
         } catch (RuntimeException e) {
             assertEquals("INVALID SHIP SIZES GIVEN", e.getMessage());
         }
