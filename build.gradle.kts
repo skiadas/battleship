a1fc2b871cceb9f1dc3b76719a9d54065d919274
Haris Skiadas <cskiadas@gmail.com>
2025-01-19 17:12:56 -0500

    Add jre-containing build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.beryx.jlink.JPackageTask
+
 plugins {
     java
     application
     id("com.diffplug.spotless") version "6.25.0"
+    id("org.beryx.jlink") version "3.1.1"
 }
 
 group = "org.example"
@@ -15,6 +18,20 @@ java {
     withJavadocJar()
     withSourcesJar()
 }
+
+jlink {
+    imageName = "${project.name}-${version}"
+    imageZip = layout.buildDirectory.file("distributions/${imageName}.zip")
+    options.addAll(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "battleship"
+    }
+}
+
+tasks.assemble {
+    dependsOn(tasks.named("jlinkZip"))
+}
+
 repositories {
     mavenCentral()
 }
@@ -23,7 +40,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.14.2")
-    implementation("org.jline:jline:3.27.1")
+//    implementation("org.jline:jline:3.27.1")
 }
 
 tasks.withType(Test::class) {
@@ -31,12 +48,12 @@ tasks.withType(Test::class) {
 }
 
 application {
-    mainClass = "Main"
+    mainClass = "main.Main"
     applicationDefaultJvmArgs += "--enable-native-access=ALL-UNNAMED"
 }
 
 tasks.named<JavaExec>("run") {
-    mainClass = "Main"
+    mainClass = "main.Main"
     standardInput = System.`in`
     jvmArgs("--enable-native-access=ALL-UNNAMED")
 }
