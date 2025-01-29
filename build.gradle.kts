9a7a4c3d3fc68266ddb805b884b12be6e2942972
Haris Skiadas <cskiadas@gmail.com>
2025-01-29 16:18:46 -0500

    Update github action (#63)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     application
     id("com.diffplug.spotless") version "6.25.0"
     id("org.beryx.jlink") version "3.1.1"
+    pmd
+    jacoco
 }
 
 group = "org.example"
@@ -36,7 +38,10 @@ repositories {
     mavenCentral()
 }
 
+val markdownDoclet by configurations.creating
+
 dependencies {
+    markdownDoclet("org.jdrupes.mdoclet:doclet:4.1.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.14.2")
@@ -65,4 +70,35 @@ spotless {
         cleanthat()
         googleJavaFormat().aosp().reflowLongStrings()
     }
+}
+
+pmd {
+    toolVersion = "6.55.0"
+    isConsoleOutput = false
+    isIgnoreFailures = true
+    ruleSetFiles(files("config/pmd.xml"))
+}
+
+tasks.javadoc {
+    options.docletpath = markdownDoclet.files.toList()
+    options.doclet = "org.jdrupes.mdoclet.MDoclet"
+    options.quiet()
+    (options as CoreJavadocOptions).addStringOption("Xdoclint:-html")
+
+    options.jFlags = listOf(
+        "--add-exports=jdk.compiler/com.sun.tools.doclint=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED",
+        "--add-exports=jdk.javadoc/jdk.javadoc.internal.tool=ALL-UNNAMED",
+        "--add-exports=jdk.javadoc/jdk.javadoc.internal.doclets.toolkit=ALL-UNNAMED",
+        "--add-opens=jdk.javadoc/jdk.javadoc.internal.doclets.toolkit.resources.releases=ALL-UNNAMED")
+}
+
+tasks.test {
+    finalizedBy(tasks.named("jacocoTestReport")) // report is always generated after tests run
+}
+tasks.named<Task>("jacocoTestReport") {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
