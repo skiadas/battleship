89db26383c4708b2a26136dc8367866f99831459
MorganChG <gatliffmorgan6@gmail.com>
2025-02-04 14:00:21 -0500

    Fixed Field Final Issue Close #86

--- src/main/java/core/ShipSpec.java
+++ src/main/java/core/ShipSpec.java
@@ -3,7 +3,9 @@ package core;
 public class ShipSpec {
     public String name;
     public int size;
+    public final String name;
 
+    public final int size;
     public ShipSpec(String name, int size) {
         this.name = name;
         this.size = size;
