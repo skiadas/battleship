6cc7850cc84d315d73756cadf622d0d80f2810b1
Oxiel <castrobadilloo27@hanover,edu>
2025-03-13 20:53:10 -0400

    Implemented CellArrayAttributeConverter with the method convertToDatabaseColumn. Also created Test class for it.

---
+++ src/test/java/ui/CellArrayAttributeConverterTest.java
@@ -0,0 +1,11 @@
+package ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CellArrayAttributeConverterTest {
+
+    @Test
+    void convertToDatabaseColumn() {}
+}
