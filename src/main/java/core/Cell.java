c2badbc7065290400c774d24a1ffae50295ed685
DelaneyChampine <champined26@hanover.edu>
2025-03-14 16:19:15 -0400

    reverted cell class to old version

--- src/main/java/core/Cell.java
+++ src/main/java/core/Cell.java
@@ -3,9 +3,20 @@ package core;
 
 public class Cell {
     private boolean shot;
+    private boolean ship;
 
     public Cell() {
         this.shot = false;
+        this.ship = false;
+    }
+
+    public Cell(boolean ship) {
+        this.ship = ship;
+        this.shot = false;
+    }
+
+    public boolean isEmpty() {
+        return !this.hasShip();
     }
 
     // the cell has been shot
@@ -13,9 +24,19 @@ public class Cell {
         return this.shot;
     }
 
+    // the cell contains a ship
+    public boolean hasShip() {
+        return this.ship;
+    }
+
     // the shot on this cell resulted in a hit
     public boolean cellIsHit() {
-        return this.hasBeenShot();
+        return this.hasBeenShot() && this.hasShip();
+    }
+
+    // shot on this cell resulted in a miss
+    public boolean cellIsMiss() {
+        return this.hasBeenShot() && !this.hasShip();
     }
 
     // Mark the cell as shot
@@ -23,8 +44,24 @@ public class Cell {
         this.shot = true;
     }
 
+    // Place a ship in the cell
+    public void setAsShip() {
+        this.ship = true;
+    }
+
+    public void setAsHit() {
+        this.ship = true;
+        this.shot = true;
+    }
+
+    public void setAsMiss() {
+        this.shot = true;
+        this.ship = false;
+    }
+
     // Reset the cell to its initial state
     public void reset() {
         this.shot = false;
+        this.ship = false;
     }
 }
