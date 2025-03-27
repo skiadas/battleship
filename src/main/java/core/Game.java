328d61243584bb48f70b51f05e19a939ad57d7f2
JonahSmith25 <156010267+JonahSmith25@users.noreply.github.com>
2025-03-27 18:34:47 +0000

    enum player added

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -1,10 +1,28 @@
 public class Game {
     private Grid first;
     private Grid second;
+    private Player current = Player.First;
 
     public Game(Grid first, Grid second) {
         this.first = first;
         this.second = second;
+    }
+
+    public enum Player {
+        First {
+            @Override
+            public Player nextPlayer() {
+                return Second;
+            }
+        },
+        Second {
+            @Override
+            public Player nextPlayer() {
+                return First;
+            }
+        };
+
+        public abstract Player nextPlayer();
     }
 
     public Grid getFirst() {
