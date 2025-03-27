31d41524c4ccd4658e29445e60b5465f5af98604
JonahSmith25 <156010267+JonahSmith25@users.noreply.github.com>
2025-03-27 18:46:46 +0000

    Current added and first test added

--- src/test/java/core/GameTest.java
+++ src/test/java/core/GameTest.java
@@ -2,4 +2,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+
+    @Test
+    public void testInitialPlayer() {
+        Grid grid1 = new Grid(); 
+        Grid grid2 = new Grid();
+        Game game = new Game(grid1, grid2);
+
+        assertEquals(Game.Player.First, game.getCurrent());
+    }
 }
