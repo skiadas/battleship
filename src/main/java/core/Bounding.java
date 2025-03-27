fc098d77c43c310b8e4afc2ace1ad39b32d284cb
Haris Skiadas <cskiadas@gmail.com>
2025-03-27 13:21:53 -0400

    Add comments to Bounding. Ref #125

--- src/main/java/core/Bounding.java
+++ src/main/java/core/Bounding.java
@@ -1,7 +1,13 @@
 package core;
 
+/**
+ * Bounding is an interface, that directs you to the Coords class rather than the Grid class it
+ * gives an easier way to access the needed and proper class.
+ */
 public interface Bounding {
+    /** numRows int value of rows in grid */
     int numRows();
 
+    /** numCols int value of Cols in grid */
     int numCols();
 }
