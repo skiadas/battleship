f0e88dc3323405c742bcb4962f4a8099d8a445cd
gav <gavenfollowell8@gmail.com>
2025-03-26 23:10:27 -0400

    Added class comments to bounding.

--- src/main/java/core/Bounding.java
+++ src/main/java/core/Bounding.java
@@ -4,6 +4,10 @@ package core;
  * to the Coords class rather than the Grid class
  * it gives an easier way to acess the needed and proper class.
 */
+/**
+ * @parma numRows int value of rows in grid
+ * @parma numCols int value of Cols in grid
+ */
 public interface Bounding {
     int numRows();
 
