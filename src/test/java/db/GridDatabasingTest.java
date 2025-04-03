a5ca54eb9fc245db7018a463c9e769e2f8794b24
MorganChG <gatliffmorgan6@gmail.com>
2025-04-02 21:46:49 -0400

    close #130 Refactored Test File

--- src/test/java/db/GridDatabasingTest.java
+++ src/test/java/db/GridDatabasingTest.java
@@ -17,28 +17,46 @@ public class GridDatabasingTest {
 
     @Test
     public void gridStoredInDatabaseAndRestoredCorrectly() {
-        EntityManager entityManager = factory.createEntityManager();
-        entityManager.getTransaction().begin();
+        Grid grid = createGridAndPersistToDatabase();
+        Grid grid2 = findGridInDataBaseAndStore(grid);
+        confirmGridsAreTheSame(grid2, grid);
+    }
+
+    private Grid createGridAndPersistToDatabase() {
+        EntityManager entityManager = createEntityManagerAndBegin();
 
         Grid grid = new Grid(5, 5, DefaultGridBuilder.defaultShipsFor5x5());
         ShipList shipList = grid.getShipListObject();
+
         entityManager.persist(shipList);
         entityManager.persist(grid);
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        commitAndCloseEntityManager(entityManager);
+        return grid;
+    }
 
-        EntityManager entityManager2 = factory.createEntityManager();
-        entityManager2.getTransaction().begin();
-
+    private Grid findGridInDataBaseAndStore(Grid grid) {
+        EntityManager entityManager2 = createEntityManagerAndBegin();
         Grid grid2 = entityManager2.find(Grid.class, grid.getId());
+        commitAndCloseEntityManager(entityManager2);
+        return grid2;
+    }
 
+    private EntityManager createEntityManagerAndBegin() {
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+        return entityManager;
+    }
+
+    private static void commitAndCloseEntityManager(EntityManager entityManager2) {
+        entityManager2.getTransaction().commit();
+        entityManager2.close();
+    }
+
+    private static void confirmGridsAreTheSame(Grid grid2, Grid grid) {
         assertNotNull(grid2);
         assertEquals(grid.numRows(), grid2.numRows());
         assertEquals(grid.numCols(), grid2.numCols());
         assertEquals(grid.getShipList().size(), grid2.getShipList().size());
-
-        entityManager2.getTransaction().commit();
-        entityManager2.close();
     }
 }
