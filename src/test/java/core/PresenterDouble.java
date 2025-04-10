e3ac56e70b5f7a660add4fe4188bd77840eb1ae2
Jonah Smith <smithj25@hanover.edu>
2025-04-10 12:39:41 -0700

    Updated Presenter and TextPresenter

--- src/test/java/core/PresenterDouble.java
+++ src/test/java/core/PresenterDouble.java
@@ -1,27 +1,41 @@
 package core;
 
+import core.state.Action;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class PresenterDouble implements Presenter {
+public class PresenterDouble extends Presenter {
     List<String> displayedMessages = new ArrayList<>();
 
+    @Override
     public void displayMessage(String s) {
         displayedMessages.add(s);
     }
 
+    @Override
     public void displayGrid(Grid g) {
         // TODO
     }
 
+    @Override
     public void displayGame(Game game) {
         // TODO
     }
 
+    @Override
     public Coord askForCoordinate(Grid g) {
         return null;
     }
 
-    public void displayOptions(String prompt, Map<String, Runnable> choices) {}
+    @Override
+    public void displayOptions(String prompt, Map<String, Runnable> choices) {
+        // simulate choice selection if needed
+    }
+
+    @Override
+    public Action askForGameAction(Grid g) {
+        // Return null for now
+        return null;
+    }
 }
